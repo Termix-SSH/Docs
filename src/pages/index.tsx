bdc0cdfe9cfaca853e1a08f874e45e55d2181a76
LukeGus <bugattiguy527@gmail.com>
2026-05-27 23:13:00 -0500

    feat: update docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--primary button--lg" to="/install">
+          <Link className="button button--primary button--lg" href="https://docs.termix.site/install">
             Install
           </Link>
         </div>
